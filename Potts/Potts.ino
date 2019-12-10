6e41ce02ff2e3b87f56b769fd967ba9855038a7b
Rob Antonisse <robantonisse@gmail.com>
2019-12-10 10:58:17 +0100

    potts

--- Potts/Potts.ino
+++ Potts/Potts.ino
@@ -1,15 +1,34 @@
 /*
  Name:		Potts.ino
  Created:	12/10/2019 10:19:31 AM
- Author:	gebruiker
+ Author:	Rob Antonisse
 */
+
+
+
+//declarations
+unsigned long puls;
 
 // the setup function runs once when you press reset or power the board
 void setup() {
-
+	DDRB |= (1 << 0); //pin8 as output
+	TCCR2B |= (1 << 0); //prescaler
+	TIMSK2 |= (1 << 0); //enable overflow interrupt
 }
 
 // the loop function runs over and over again until power down or reset
-void loop() {
-  
+
+ISR(TIMER2_OVF_vect) {
+	PINB |= (1 << 0);
 }
+void loop() {
+	
+	//
+
+	if (micros() - puls > 1) {
+		PINB |= (1 << 0);
+		puls = micros();
+	}
+	//
+}
+
