5060051486cecea0fdffc3408819ada5e22cf7ea
Rob Antonisse <robantonisse@gmail.com>
2019-12-21 22:07:30 +0100

    RC522 testen

--- RFID-RC522/RFID-RC522.ino
+++ RFID-RC522/RFID-RC522.ino
@@ -4,124 +4,62 @@
  Author:	rob Antonisse
  Basis gebruik van RFID card alleen als toegang, identificatie. Storage op de card en verdere trucken niet bekeken.
  Gebruikt MFRC522 library BY COOQROBOT
- Als basis example: READNUID
+
+ Diverse testen mee te doen, bv. meerdere kaarten lezen
+
 */
 
 #include <SPI.h>
 #include <MFRC522.h>
-
-#define SS_PIN 10  // slave select pin SPI
 #define RST_PIN 9 //reset pin SPI
+MFRC522 reader[2];   // Create MFRC522 instance.
+byte sspin[2] = { 10,8 };  //slave select pins
 
-MFRC522 rfid(SS_PIN, RST_PIN); // Instance of the class
 
-///***** alleen teksten, soort kaart weergave
-//MFRC522::MIFARE_Key key; //defineer een key
-///alleen voor teksten
-
-// Init array that will store new NUID 
-byte nuidPICC[4];
-
-byte akkoord[4] = { 0xD9, 0x8F, 0x27, 0xA3 };
-
-unsigned long periode;
 void setup() {
 	Serial.begin(9600);
 	SPI.begin(); // Init SPI bus
-	rfid.PCD_Init(); // Init MFRC522 
 
-
-	/*  waar is dit voor, volgens mij alleen teksten
-	for (byte i = 0; i < 6; i++) {
-		key.keyByte[i] = 0xFF;
+	for (uint8_t card = 0; card < 2; card++) {
+		reader[card].PCD_Init(sspin[card], RST_PIN); // Init each MFRC522 card
+		Serial.print(F("Reader "));
+		Serial.print(card);
+		Serial.print(F(": "));
+		reader[card].PCD_DumpVersionToSerial();
 	}
-
-	Serial.println(F("This code scan the MIFARE Classsic NUID."));
-	Serial.print(F("Using the following key:"));
-	printHex(key.keyByte, MFRC522::MF_KEY_SIZE);
-*/
 }
 
 void loop() {
-	if (millis() - periode > 100) { //make periodic read of cards
-		periode = millis();
+	for (byte card = 0; card < 2; card++) {		
 
-		// Reset the loop if no new card present on the sensor/reader. This saves the entire process when idle.			
-		if (!rfid.PICC_IsNewCardPresent()) return;   //rfid.PICC_IsNewCardPresent(); //nodig wakes up the card
-	
-		// Verify if the NUID has been readed
-		if (!rfid.PICC_ReadCardSerial())return;  //rfid.PICC_ReadCardSerial(); //ook nodig, als true dan is UID en SAK gelezen
+		if (reader[card].PICC_IsNewCardPresent() && reader[card].PICC_ReadCardSerial()) { // Look for new cards
+			Serial.print(F("Reader "));
+			Serial.print(card);
+			// Show some details of the PICC (that is: the tag/card)
+			Serial.print(F(": Card UID:"));
 
-		/*
-		/////alleen voor tekst
-		Serial.print(F("PICC type: "));			   
-		MFRC522::PICC_Type piccType = rfid.PICC_GetType(rfid.uid.sak);
-		Serial.println(rfid.PICC_GetTypeName(piccType));
+			//reader[card].uid.uidByte;
 
-		// Check is the PICC of Classic MIFARE type
-		if (piccType != MFRC522::PICC_TYPE_MIFARE_MINI &&
-			piccType != MFRC522::PICC_TYPE_MIFARE_1K &&
-			piccType != MFRC522::PICC_TYPE_MIFARE_4K) {
-			Serial.println(F("Your tag is not of type MIFARE Classic."));
-			return;
-		}
-		/////alleen voor tekst
-		*/
+			for (byte i = 0; i < reader[card].uid.size; i++) {
+				Serial.print(reader[card].uid.uidByte[i] < 0x10 ? " 0" : " ");
+				Serial.print(reader[card].uid.uidByte[i], HEX);
+			}
 
 
+			//dump_byte_array(reader[card].uid.uidByte, reader[card].uid.size);
+			Serial.println();
+			Serial.print(F("PICC type: "));
+			MFRC522::PICC_Type piccType = reader[card].PICC_GetType(reader[card].uid.sak);
+			Serial.println(reader[card].PICC_GetTypeName(piccType));			
+			reader[card].PICC_HaltA();// Halt PICC			
+			reader[card].PCD_StopCrypto1();// Stop encryption on PCD
+		} //if (reader[card].PICC_IsNewC
+	} //for(byte reader
 
-		//****Zorg dat alleen een nieuwe card wordt gelezen
-
-		//if (rfid.uid.uidByte[0] != nuidPICC[0] ||
-			//rfid.uid.uidByte[1] != nuidPICC[1] ||
-			//rfid.uid.uidByte[2] != nuidPICC[2] ||
-			//rfid.uid.uidByte[3] != nuidPICC[3]) {
-			//Serial.println(F("A new card has been detected."));
-			// Store NUID into nuidPICC array
-		//for (byte i = 0; i < 4; i++) {
-		//	nuidPICC[i] = rfid.uid.uidByte[i];
-		//}
-
-
-		Serial.println(F("The NUID tag is:"));
-		Serial.print(F("In hex: "));
-		printHex(rfid.uid.uidByte, rfid.uid.size);
-		Serial.println();
-		Serial.print(F("In dec: "));
-		printDec(rfid.uid.uidByte, rfid.uid.size);
-		Serial.println();
-		//}
-		//else Serial.println(F("Card read previously."));
-
-		// Halt PICC
-		rfid.PICC_HaltA(); //set de card is pauze, nu wordt niet telkens deze card gelezen, pas als de card
-		//stroomloos is geweest wordt het opnieuw herkent.
-
-		// Stop encryption on PCD
-		//rfid.PCD_StopCrypto1();
-	}
 }
-
-
-
-
- //Helper routine to dump a byte array as hex values to Serial.
-
-void printHex(byte *buffer, byte bufferSize) {
+void dump_byte_array(byte *buffer, byte bufferSize) {
 	for (byte i = 0; i < bufferSize; i++) {
-		Serial.print(buffer[i] < 0x10 ? " 0" : " ");
-		Serial.print(buffer[i], BIN);
-	}
-}
-
-/**
- * Helper routine to dump a byte array as dec values to Serial.
- */
-void printDec(byte *buffer, byte bufferSize) {
-	for (byte i = 0; i < bufferSize; i++) {
-		Serial.print(buffer[i] < 0x10 ? " 0" : " ");
+		//Serial.print(buffer[i] < 0x10 ? " 0" : " ");
 		Serial.print(buffer[i], DEC);
 	}
-
-
 }
